9db5909262d826014ddf3ac20004b9f67859b1aa
feralf9 <uelonestar@gmail.com>
2026-08-04 12:06:37 -0400

    Refactor item object structure in temp.js

--- temp.js
+++ temp.js
@@ -9,28 +9,25 @@ value: 0,
 age: 0,
 in_use: false,
 in_working_condition: false
-
 },{
 item: "one small drop kiln",
 owner: "Scott",
-studio_id: "sc-DK-02"
-location: "?"
-ref_link: "https://example.com/3"
-value: 0
-age: 0
-in_use: false
+studio_id: "sc-DK-02",
+location: "?",
+ref_link: "https://example.com/3",
+value: 0,
+age: 0,
+in_use: false,
 in_working_condition: false
-
 },{
-
-item: "paragon small kiln"
-owner: "Alex French"
-studio_id: "AF-SK-03"
-location: "jewelry studio room"
-ref_link: "https://example.com/3"
-value: 0
-age: 0
-in_use: false
+item: "paragon small kiln",
+owner: "Alex French",
+studio_id: "AF-SK-03",
+location: "jewelry studio room",
+ref_link: "https://example.com/3",
+value: 0,
+age: 0,
+in_use: false,
 in_working_condition: false
 }
 
