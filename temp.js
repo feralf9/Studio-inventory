c749c62e9a1f95e9e9e7d37914f900ebbbef0d40
feralf9 <uelonestar@gmail.com>
2026-08-04 12:05:25 -0400

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,35 +1,35 @@
 Database.register([
 {
-item: wet dog drop kiln
-owner: Jessie zwicky
-studio_id: JZ-DK-01
-location: by east facing garage door
-ref_link: https://example.com/3
+item: "wet dog drop kiln",
+owner: "Jessie zwicky",
+studio_id: "JZ-DK-01",
+location: "by east facing garage door",
+ref_link: "https://example.com/3",
+value: 0,
+age: 0,
+in_use: false,
+in_working_condition: false
+
+},{
+item: "one small drop kiln",
+owner: "Scott",
+studio_id: "sc-DK-02"
+location: "?"
+ref_link: "https://example.com/3"
 value: 0
-age: o
+age: 0
 in_use: false
 in_working_condition: false
 
 },{
-item: one small drop kiln
-owner: Scott
-studio_id: sc-DK-02
-location: ?
-ref_link: https://example.com/3
-value: 0
-age: o
-in_use: false
-in_working_condition: false
 
-},{
-
-item: paragon small kiln
-owner: Alex French
-studio_id: AF-SK-03
-location: jewelry studio room
-ref_link: https://example.com/3
+item: "paragon small kiln"
+owner: "Alex French"
+studio_id: "AF-SK-03"
+location: "jewelry studio room"
+ref_link: "https://example.com/3"
 value: 0
-age: o
+age: 0
 in_use: false
 in_working_condition: false
 }
